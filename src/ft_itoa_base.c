32bdeb6845c051cb6e1012e338b026b458b1b01a
Kevin Erbault <kerbault@z2r7p1.le-101.fr>
2018-01-09 11:28:01 +0100

    h

--- src/ft_itoa_base.c
+++ src/ft_itoa_base.c
@@ -6,14 +6,41 @@
 /*   By: kerbault <kerbault@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/01/08 20:37:07 by kerbault     #+#   ##    ##    #+#       */
-/*   Updated: 2018/01/09 10:22:32 by kerbault    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/01/09 11:25:47 by kerbault    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
-#include <stdlib.h>
+#include "libft.h"
+
+size_t	mall_size(int nb, int base)
+{
+	size_t	i;
+	int		tmp;
+
+	i = 1;
+	while (nb / base)
+	{
+		tmp = nb / base;
+		i++;
+	}
+	return (i);
+}
 
 char	*ft_itoabase(int nb, int base)
 {
-	test
+	char	*ret;
+	char	*index;
+	int		tmp;
+
+	if (base == 10)
+		return (ft_itoa(nb));
+	if (nb == 0)
+		return (ft_strdup("0\0"));
+	index = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	index = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	
+	return (ret);
+	ft_itoa()
+	
 }
