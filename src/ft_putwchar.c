d01bafbb25d9f142f151ea9723245372bf196249
Kevin Erbault <kerbault@z2r6p2.le-101.fr>
2018-03-01 09:27:59 +0100

    wchat

--- src/ft_putwchar.c
+++ src/ft_putwchar.c
@@ -6,17 +6,42 @@
 /*   By: kerbault <kerbault@student.le-101.fr>      +:+   +:    +:    +:+     */
 /*                                                 #+#   #+    #+    #+#      */
 /*   Created: 2018/02/12 19:10:05 by kerbault     #+#   ##    ##    #+#       */
-/*   Updated: 2018/02/12 19:31:42 by kerbault    ###    #+. /#+    ###.fr     */
+/*   Updated: 2018/03/01 08:48:52 by kerbault    ###    #+. /#+    ###.fr     */
 /*                                                         /                  */
 /*                                                        /                   */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int		ft_putwchar(wchar_t c)
+int		ft_putwchar(wchar_t wc)
 {
-	size_t	ret;
+	int	ret;
 
-	ret = 0;
+	if (wc <= 127)
+	{
+		ft_putchar(wc);
+		ret = 1;
+	}
+	else if (wc <= 2047)
+	{
+		ft_putchar((wc >> 6) + 0xC0);
+		ft_putchar((wc & 0x3F) + 0x80);
+		ret = 2;
+	}
+	else if (wc <= 65535)
+	{
+		ft_putchar((wc >> 12) + 0xE0);
+		ft_putchar(((wc >> 6) & 0x3F) + 0x80);
+		ft_putchar((wc & 0x3F) + 0x80);
+		ret = 3;
+	}
+	else if (wc <= 1114111)
+	{
+		ft_putchar((wc >> 18) + 0xF0);
+		ft_putchar(((wc >> 12) & 0x3F) + 0x80);
+		ft_putchar(((wc >> 6) & 0x3F) + 0x80);
+		ft_putchar((wc & 0x3F) + 0x80);
+		ret = 4;
+	}
 	return (ret);
 }
